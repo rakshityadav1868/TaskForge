233b062a736e4b67c4442a927093b692e5b6c2fc
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-06-13 02:03:13 +0530

    created basic sql query for db table
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

--- autoworkers/internal/database/database.go
+++ autoworkers/internal/database/database.go
@@ -14,11 +14,21 @@ type Database struct {
 
 func Constructor() *Database{
 	db, err := sql.Open("sqlite", "./taskforge.db")
+	query := `
+	CREATE TABLE Jobs(
+	id TEXT,
+	type TEXT,
+	payload TEXT,
+	status INTEGER,
+	result TEXT
+	)
+	`
 	fmt.Println("after open", err)
 	if err != nil {
 		fmt.Println(err)
 	}
 	err =db.Ping()
+	_, err = db.Exec(query)
 	if err!=nil{
 		 fmt.Println(err)
 	}else{
